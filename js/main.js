dd91530eb49627201cdb187de17f0d5e3324d385
Lucas <gasparlucas8@gmail.com>
2023-10-27 21:48:28 -0300

    Estilo list item terminado

--- js/main.js
+++ js/main.js
@@ -4,16 +4,38 @@ const body = document.querySelector('body')
 // BOTONES
 const cancelar = document.querySelector('#cancelar')
 const agregar = document.querySelector('#agregar')
+// INPUTS
+const nombre = document.querySelector('#nombre')
+const tipodetarea = document.querySelector('#tipodetarea')
+const descripcion = document.querySelector('#descripcion')
+// LISTA
+const lista = document.querySelector('#lista')
 
-
-
+// BOTON MOSTRAR POPUP
 mostrarPopup.addEventListener('click', () => {
     popup.classList.remove('show');
     body.classList.add('body-style')
     mostrarPopup.style.cursor = 'auto'
 })
+// BOTON CANCELAR SE CIERRA EL POPUP
 cancelar.addEventListener('click', () => {
     popup.classList.add('show')
     body.classList.remove('body-style')
     mostrarPopup.style.cursor = 'pointer'
 })
+
+// LOGICA AGREGAR TAREA
+const agregarTarea = () => {
+    if ( nombre.value != '' && tipodetarea.value != 'Elegir tipo' && descripcion.value != '' && (check1.checked == true || check2.checked == true || check3.checked == true || check4.checked == true)) {
+        const item = document.createElement('li')
+        const p = document.createElement('li')
+        p.textContent = 'hola que tal'
+        item.appendChild(p)
+        lista.appendChild(item)
+    }
+    else{
+        console.log('no funciona')
+    }
+}
+
+agregar.addEventListener('click', agregarTarea)
